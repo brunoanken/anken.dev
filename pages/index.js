f60c11caaeb61d2a8988c287440948af98e5a141
brunoanken <brunoanken@gmail.com>
2020-06-23 21:26:08 -0300

    :bento: :lipstick: Start styling the home page. Add my profile picture, alongside some idiot text.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from './styles/index.module.scss'
 
 export default function Home() {
   return (
@@ -8,8 +9,33 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>Anken</h1>
+      <main className="container column">
+
+        <div className="container padding">
+          <img src="/images/profile.jpg" alt="Eu, Bruno Anken" className="round-border" />
+        </div>
+
+        <h1 className="text-center">
+          Olá, eu sou o Bruno Anken
+        </h1>
+        <p className={`${styles['main-text']} text-center padding`}>
+          Dono de uma voz exagaradamente sexy, estou pronto para gemer a qualquer momento
+        </p>
+
+        <div className="bg-secondary padding text-center">
+          <h2>"Tá, legal, mas pra que você serve?"</h2>
+          <br />
+          <p>Se liga, mané!</p>
+          <br />
+
+          <div className={styles['sections-grid']}>
+            <p className="w">Pinto</p>
+            <p className="w">Pinto</p>
+            <p className="w">Pinto</p>
+            <p className="w">Pinto</p>
+          </div>
+        </div>
+
       </main>
     </div>
   )
