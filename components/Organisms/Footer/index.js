f243b05d8f6c0340daba590cf8104c8e2a06c5d3
brunoanken <brunoanken@gmail.com>
2020-06-25 17:20:28 -0300

    :lipstick: Add GitHub icon and link in footer. Update its background-color.

--- components/Organisms/Footer/index.js
+++ components/Organisms/Footer/index.js
@@ -10,6 +10,7 @@ const Footer = () => (
             <a href="https://www.linkedin.com/in/brunoanken/"><img src="/icons/linkedin.svg" alt="LinkedIn" /></a>
             <a href="https://www.instagram.com/brunoanken/"><img src="/icons/instagram.svg" alt="Instagram" /></a>
             <a href="https://twitter.com/Zaninellossauro"><img src="/icons/twitter.svg" alt="Twitter" /></a>
+            <a href="https://twitter.com/Zaninellossauro"><img src="/icons/github.svg" alt="Twitter" /></a>
         </div>
     </div>
 )
