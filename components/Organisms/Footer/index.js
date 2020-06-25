4f8f91fe412cf914fec7d0cc2b61170a67fa0c92
brunoanken <brunoanken@gmail.com>
2020-06-25 12:02:21 -0300

    :bento: Add Roboto font to the website.

--- components/Organisms/Footer/index.js
+++ components/Organisms/Footer/index.js
@@ -3,7 +3,7 @@ import styles from './footer.module.scss'
 const Footer = () => (
     <div className={`${styles['container']} padding text-center`}>
         <p>
-            Feito por Bruno Anken usando <img src="/icons/react.svg" alt="React" />
+            Feito por Bruno Anken
         </p>
     </div>
 )
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Footer from '../components/Organisms/Footer'
 
 import '../styles/global.scss'
@@ -5,6 +6,9 @@ import '../styles/global.scss'
 const App = ({ Component, pageProps }) => {
     return (
         <>
+            <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,900;1,400;1,900&display=swap" rel="stylesheet" />
+            </Head>
             <Component {...pageProps} />
             <Footer />
         </>
