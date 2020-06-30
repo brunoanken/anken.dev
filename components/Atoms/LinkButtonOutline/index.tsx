36afdec91a0f17baa1b27ff67c595b18bbacbda8
brunoanken <brunoanken@gmail.com>
2020-06-30 18:25:44 -0300

    :label: Add TS to LinkButtonOutline atom component.

--- components/Atoms/LinkButtonOutline/index.tsx
+++ components/Atoms/LinkButtonOutline/index.tsx
@@ -1,15 +1,13 @@
-import PropTypes from 'prop-types'
-
 import styles from './styles.module.scss'
+
+type Props = {
+    children: React.ReactNode,
+    href: string,
+    download: boolean
+}
 
 const LinkButtonOutline = ({ children, href, download = false }) => (
     <a href={href} download={download} className={styles['container']}>{children}</a>
 )
-
-LinkButtonOutline.propTypes = {
-    children: PropTypes.node.isRequired,
-    href: PropTypes.string.isRequired,
-    download: PropTypes.bool
-}
 
 export default LinkButtonOutline
